ad4a44c5bd00a7b58788823041b15a07aeb66369
Franzess Karel T. Honoridez <karelhonoridez@gmail.com>
2026-03-16 01:42:53 +0800

    Add middleware to log incoming requests

--- index.js
+++ index.js
@@ -4,8 +4,21 @@ const port = 3000;
 
 // Serve static files from the "public" folder
 app.use(express.static('public'));
+app.use(express.json());
+
+// Logging middleware - must be BEFORE routes
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+});
+
 app.get('/about', (req, res) => {
     res.send('About Us');
+});
+
+app.post('/submit', (req, res) => {
+    const data = req.body;
+    res.send(`Received: ${JSON.stringify(data)}`);
 });
 
 // Define a route for the home page
